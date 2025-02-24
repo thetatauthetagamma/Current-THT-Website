c6405362060d070ac091e463b4855dafa126120e
katemcg <katemcg@umich.edu>
2025-02-23 21:54:27 -0500

    Literally just changing button colors

--- components/admin/RushAdminPanel.jsx
+++ components/admin/RushAdminPanel.jsx
@@ -525,7 +525,7 @@ export default function RushAdminPanel() {
           </button>
           <button
             onClick={handleEliminateOthers}
-            className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
+            className="bg-[#8B0000]text-white px-4 py-2 rounded hover:bg-red-800"
           >
             Eliminate Others
           </button>
@@ -549,7 +549,7 @@ export default function RushAdminPanel() {
       <div className="mt-8 p-4 border rounded bg-gray-50">
       <button
             onClick={handleFinalizePC}
-            className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
+            className="bg-[#8B0000] text-white px-4 py-2 rounded hover:bg-red-800"
           >
             Finalize PC!!!!!!!!!! 
           </button>
